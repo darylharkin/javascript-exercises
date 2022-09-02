c91514e4d930b62899435bdc79d02843f15b74b8
daryl <darylsharkin@gmail.com>
2022-09-02 12:48:03 +0100

    saving

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let output = ""
+    for (let i = 0; i < num; i++) {
+        output += string
+    } 
+    return output
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrTemp) {
+  let convOne = (fahrTemp - 32) / 1.8
+  convOne = Math.round(convOne * 10) / 10
+  return convOne;
 };
 
-const ctof = function() {
-
+const ctof = function(celsTemp) {
+  let convTwo = (celsTemp * 1.8) + 32
+  convTwo = Math.round(convTwo * 10) / 10
+  return convTwo
 };
 
 // Do not edit below this line
